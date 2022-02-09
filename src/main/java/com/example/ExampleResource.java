8510deccecd89519de06e0877e3d0273334e06c3
user <user@example.com>
2022-02-09 23:22:33 +0100

    get and respond with git commit id

--- src/main/java/com/example/ExampleResource.java
+++ src/main/java/com/example/ExampleResource.java
@@ -4,6 +4,9 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 @Path("/hello")
 public class ExampleResource {
@@ -11,6 +14,18 @@ public class ExampleResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello RESTEasy";
+        final var resourceAsStream = ExampleResource.class.getResourceAsStream("/git.properties");
+        final var properties = new Properties();
+        try {
+            System.out.println("resourceAsStream = " + resourceAsStream);
+            properties.load(resourceAsStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("properties = " + properties);
+        final var commitId = properties.getProperty("git.commit.id");
+        var closestTagName = properties.getProperty("git.closest.tag.name");
+        if(closestTagName.equals("")) closestTagName = "no tag";
+        return "Hello! commit id: " + commitId + ", closest tag: " + closestTagName;
     }
 }
